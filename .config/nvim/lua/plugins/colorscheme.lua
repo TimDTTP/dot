ecb454d7533d1a1cd95bb528816060bd30a96916
TimDTTP <timdttp@gmail.com>
2025-03-27 17:13:20 -0500

    update: migration to catppuccin and integration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"EdenEast/nightfox.nvim",
+	"catppuccin/nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("carbonfox")
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
