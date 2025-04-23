d0b695668a60d66ab6bb10f1c0360196aeee356e
TimDTTP <timdttp@gmail.com>
2025-04-23 14:21:54 -0500

    test

---
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		direction = "float",
+		float_opts = {
+			border = "curved",
+			winblend = 0,
+			width = 130,
+			height = 30,
+		},
+	},
+}
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 13
+config.color_scheme = "Catppuccin Mocha"
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.term = "xterm-256color"
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 50
+config.max_fps = 60
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+local act = wezterm.action
+
+config.keys = {
+	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-5) },
+	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(5) },
+}
+
+-- and finally, return the configuration to wezterm
+return config
